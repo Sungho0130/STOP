4bac7d98143f451bc5459e8cc51dbda4e50a7d9f
DY <changu0903@gmail.com>
2023-09-22 22:22:47 +0900

    # 태블로 결과 Django 연동 - 대시보드를 연결할 detail_page.html 작성 - 해당 페이지를 보여줄 urls.py 와 views.py 작성

--- stop/see_data/urls.py
+++ stop/see_data/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.main_page, name="main_page"),
-
+    path("detail/", views.detail_page, name="detail_page"),
 ]
--- stop/see_data/views.py
+++ stop/see_data/views.py
@@ -4,3 +4,6 @@ from .models import Post
 
 def main_page(request):
     return render(request, "page/main_page.html")
+
+def detail_page(request):
+    return render(request, "page/tableau_detail.html")

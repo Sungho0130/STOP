38ea8667fe2cfb6ade121584190d8e9d3ee80169
Choi Sungho <starhochoi@gmail.com>
2023-09-25 16:04:13 +0900

    - 누락 코드 추가

--- stop/see_data/urls.py
+++ stop/see_data/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.main_page, name="main_page"),
     path("detail/", views.detail_page, name="detail_page"),
+    path("analysis/", views.detail_page, name="analysis_page"),
+    path("cause_of/", views.detail_page, name="cause_of_page"),
+    path("preventive/", views.detail_page, name="preventive_page"),
 ]
--- stop/see_data/views.py
+++ stop/see_data/views.py
@@ -7,3 +7,12 @@ def main_page(request):
 
 def detail_page(request):
     return render(request, "page/tableau_detail.html")
+
+def analysis_page(request):
+    return render(request, "page/analysis.html")
+
+def cause_of_page(request):
+    return render(request, "page/cause_of.html")
+
+def preventive_page(request):
+    return render(request, "page/preventive.html")

ab7d1eb0356f8c95cce4e6d0f4cabd319d42044c
Choi Sungho <starhochoi@gmail.com>
2023-09-26 12:10:28 +0900

    url view 코드실수 재지정

--- stop/see_data/urls.py
+++ stop/see_data/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.main_page, name="main_page"),
     path("detail/", views.detail_page, name="detail_page"),
-    path("analysis/", views.detail_page, name="analysis_page"),
-    path("cause_of/", views.detail_page, name="cause_of_page"),
-    path("preventive/", views.detail_page, name="preventive_page"),
+    path("analysis/", views.analysis_page, name="analysis_page"),
+    path("cause_of/", views.cause_of_page, name="cause_of_page"),
+    path("preventive/", views.preventive_page, name="preventive_page"),
     path("ask/", views.ask_page, name="ask_page"),
 ]

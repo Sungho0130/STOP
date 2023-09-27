31a957e0474508ed36225c1aeb44d9dcc19d1e82
Oh YoungHwan <ddackpool@naver.com>
2023-09-27 10:27:21 +0900

    데이터 시각화 페이지 태블로랑 연동. url 수정

--- stop/see_data/urls.py
+++ stop/see_data/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", views.main_page, name="main_page"),
     path("detail/", views.detail_page, name="detail_page"),
     path("analysis/", views.detail_page, name="analysis_page"),
-    path("cause_of/", views.detail_page, name="cause_of_page"),
-    path("preventive/", views.detail_page, name="preventive_page"),
+    path("cause_of/", views.cause_of_page, name="cause_of_page"),
+    path("preventive/", views.preventive_page, name="preventive_page"),
     path("ask/", views.ask_page, name="ask_page"),
 ]
